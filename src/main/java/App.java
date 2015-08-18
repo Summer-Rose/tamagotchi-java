e33dd7608a1780c7b42837c5047d2d26a59b14b9
Summer Brochtrup & Momo Ozawa <student@epicodus.com>
2015-08-18 12:25:55 -0700

    add unit tests and logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,35 @@
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
-import static spark.Spark.*;
+// import java.util.ArrayList;
+// import java.util.Map;
+// import java.util.HashMap;
+// import spark.ModelAndView;
+// import spark.template.velocity.VelocityTemplateEngine;
+// import static spark.Spark.*;
 
 
 public class App {
-    public static void main(String[] args) {
-    	staticFileLocation("/public");
-    	String layout = "templates/layout.vtl";
+    public static Tamagotchi momo = new Tamagotchi("Momo");
 
-        
+    public static void main(String[] args) {
+    	// staticFileLocation("/public");
+    	// String layout = "templates/layout.vtl";
+      System.out.println("Initial weight: " + momo.getWeight());
+      System.out.println("**********************************");
+
+      for (int i=0; i<50; i++) {
+        eatMeals();
+      }
+
+      momo.getOlder();
+      System.out.println("**********************************");
+      System.out.println("Age: " + momo.getAge());
+
+    }
+
+    public static void eatMeals() {
+      System.out.println("Calling eat...");
+      momo.eat();
+      System.out.println("Number of meals eaten: " + momo.getMeals());
+      System.out.println("Weight: " + momo.getWeight());
+      System.out.println("**********************************");
     }
 }
--- src/main/java/Tamagotchi.java
+++ src/main/java/Tamagotchi.java
@@ -7,10 +7,17 @@ public class Tamagotchi {
 	private int mBathroomLevel;
 	private int mTime;
 
+	private final int MAX_MEALS = 5;
+	private final int MAX_HAPPYLEVEL = 5;
+	private final int MAX_BATHROOMLEVEL = 5;
+
 	public Tamagotchi(String name) {
 		mName = name;
 		mWeight = 1;
 		mAge = 0;
+		mMeals = 0;
+		mHappyLevel = 3;
+		mBathroomLevel = 0;
 	}
 
 	public String getName() {
@@ -22,6 +29,35 @@ public class Tamagotchi {
 	}
 
 	public int getAge() {
+		return mAge;
+	}
+
+	public int getMeals() {
+		return mMeals;
+	}
+
+	public int getHappyLevel() {
+		return mHappyLevel;
+	}
+
+	public int getBathroomLevel() {
+		return mBathroomLevel;
+	}
+
+	public int eat() {
+		if (mMeals < MAX_MEALS) {
+			mMeals += 1;
+		} else if (mMeals == MAX_MEALS){
+			mWeight += 1;
+			mMeals = 0;
+		}
+		return mMeals;
+	}
+
+	public int getOlder() {
+		if (mWeight % 5 == 0) {
+			mAge += (mWeight/5) + 1;
+		}
 		return mAge;
 	}
 }
