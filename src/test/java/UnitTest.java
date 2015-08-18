17b6b87b4befd7b42c243d009070c530ca0543dc
Summer Brochtrup <sbrochtrup@gmail.com>
2015-08-17 22:23:10 -0700

    Initial commit

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -3,5 +3,16 @@ import static org.junit.Assert.*;
 
 public class UnitTest {
 
+	@Test
+	public void Task_instantiatesCorrectly_true() {
+		Task myTask = new Task("Mow the lawn");
+		assertEquals(true, myTask instanceof Task);
+	}
+
+	@Test
+	public void task_instantiatesWithDescription_true() {
+		Task myTask = new Task("Mow the lawn");
+		assertEquals("Mow the lawn", myTask.getDescription());
+	}
 
 }
