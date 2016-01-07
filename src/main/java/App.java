b7d070ac206235d12bd209c46e4c8acf048baced
Summer Brochtrup <sbrochtrup@gmail.com>
2016-01-07 14:42:58 -0800

    prepare to deploy to Heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,17 @@ public class App {
     public static String userName = "";
     public static Tamagotchi myTam;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+     setPort(port);
+
     	staticFileLocation("/public");
     	String layout = "templates/layout.vtl";
 
